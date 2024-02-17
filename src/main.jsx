ca0951fdd9c120baaab2f01f5462e1577c181d35
DoodPop <117036115+DoodPop@users.noreply.github.com>
2024-02-17 16:33:30 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,7 @@ import App from './App.jsx'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-   <BrowserRouter basename={"/<Synonym-Game>/"}>
+  <BrowserRouter basename="/<Synonym-Game>/">
     <App />
-  
+  </BrowserRouter> 
 );
-      
