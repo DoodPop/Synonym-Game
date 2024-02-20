f20de883be5551f507791f26bd396330749991c1
DoodPop <117036115+DoodPop@users.noreply.github.com>
2024-02-19 18:27:10 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/<Synonym-Game>/">
     <App />
   </BrowserRouter> 
 );
+reportWebVitals();
